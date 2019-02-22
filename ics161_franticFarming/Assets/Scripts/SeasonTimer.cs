f8e232469374e377f5bc3b70812ce0d51dc7f09b
Marcelo Pulido <mipulido@uci.edu>
2019-02-21 16:57:27 -0800

    Added season timer script, added TextMeshPro tools
    - Added the SeasonTimer script to the project, it basically runs a coroutine timer and keeps track of the seasons.
    
    - Added the TextMeshPro tools to the project. This is really to be used for later when we create and implement our UI.

---
+++ ics161_franticFarming/Assets/Scripts/SeasonTimer.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum Season
+{
+    winter, spring, summer, fall
+}
+
+public class SeasonTimer : MonoBehaviour
+{
+    public int secondsPerSeason = 15;
+
+    private int time;
+    private Season currentSeason;
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI seasonText;
+
+    void Awake()
+    {
+        time = 0;
+        currentSeason = Season.winter;
+    }
+
+    void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Private functions
+    private void CheckForNewSeason()
+    {
+        int elapsedSeasons = time / secondsPerSeason;
+
+        switch (elapsedSeasons % 4)
+        {
+            case 0:
+                currentSeason = Season.winter;
+                break;
+            case 1:
+                currentSeason = Season.spring;
+                break;
+            case 2:
+                currentSeason = Season.summer;
+                break;
+            case 3:
+                currentSeason = Season.fall;
+                break;
+            default:
+                throw new System.Exception("No condition set for currentSeason = " + (elapsedSeasons % 4));
+        }
+    }
+
+    private void UpdateUI()
+    {
+        timerText.text = time.ToString();
+        seasonText.text = UppercaseFirstLetter(currentSeason.ToString());
+    }
+
+    private string UppercaseFirstLetter(string word)
+    {
+        if (word != null)
+        {
+            switch (word.Length)
+            {
+                case 1:
+                    return word.ToUpper();
+                default:
+                    return word.Substring(0, 1).ToUpper() + word.Substring(1);
+            }
+        }
+        throw new System.Exception("Could not uppercase string; given string is null.");
+    }
+
+    // Public functions
+    public int GetTime()
+    {
+        return time;
+    }
+
+    public Season GetCurrentSeason()
+    {
+        return currentSeason;
+    }
+
+    // Coroutines
+    IEnumerator StartTimer()
+    {
+        while (true)
+        {
+            CheckForNewSeason();
+            UpdateUI();
+
+            yield return new WaitForSeconds(1f);                    // Makes coroutine wait for 1 in-game second
+
+            ++time;                                                 // Increment time by 1
+        }
+    }
+}
