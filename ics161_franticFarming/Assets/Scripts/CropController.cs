9d24fbb0420f9ea2597e9138ff0acfcd30e167ed
fortiz40 <fortiz40.fo@gmail.com>
2019-03-04 18:43:43 -0800

    Updated crop and added cropTile v2 prefab

--- ics161_franticFarming/Assets/Scripts/CropController.cs
+++ ics161_franticFarming/Assets/Scripts/CropController.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
 
 public class CropController : MonoBehaviour
 {
-    
+
+    private bool DEBUG = true; // Change to see debug messages or not
+
     private SpriteRenderer currentCropStatusSpriteRenderer;
 
     [SerializeField] private Sprite noCropSprite;
     [SerializeField] private Sprite[] plantedCropSprites;
     [SerializeField] private Sprite matureCropSprite;
 
+    private TextMeshProUGUI maturityText;
+    private TextMeshProUGUI fertilizationText;
+
     private bool isPlanted;
     private bool isAlive;
     private bool isMature;
+    private bool isFamished;
 
     private Season currentSeason;
+
+    [SerializeField] private float famishCropChance = 0.05f;
+    [SerializeField] private float crowAppearChance = 0.25f;
 
     public int Maturity { get; private set; } = 0;
     public float Fertilization = 0;
@@ -23,8 +35,10 @@ public class CropController : MonoBehaviour
     [SerializeField] private int cropMaturityLevel = 20; // Number to indicate when a crop is fully mature
 
     [SerializeField] private int maturityIncreaseRate = 2 ;  // Normal rate for crop maturity growth when water = 0
-    [SerializeField] private int fertilizedIncreaseRate = 1; // Added rate when crop has > 0 Fertilization
-    [SerializeField] private int fertilizedDecreaseRate = 1; // Decreases mautirity growth when Fertilization = 0
+
+    //[SerializeField] private int fertilizedIncreaseRate = 2; // Added rate when crop has > 0 Fertilization
+    //[SerializeField] private int fertilizedDecreaseRate = 1; // Decreases mautirity growth when Fertilization = 0
+
     [SerializeField] private float baseFertilizerAddAmount = 50; //Amount to increase Fertilization when crop is clicked
     [SerializeField] private int dropFertilizationRate = 1; // Amount to drop fertilization by each second
     [SerializeField] private float updateCropSeconds = 1.0f; // Number of seconds to wait between each UpdateCropStatus call
@@ -37,6 +51,7 @@ public class CropController : MonoBehaviour
         isPlanted = false;
         isAlive = false;
         isMature = false;
+        isFamished = false;
 
         Maturity = 0;
         Fertilization = 0;
@@ -50,7 +65,33 @@ public class CropController : MonoBehaviour
         SeasonTimer.instance.m_SeasonChange.AddListener(OnSeasonChangeListener);
         currentSeason = SeasonTimer.instance.GetCurrentSeason();
 
-        player = GameObject.FindObjectOfType<Player>().GetComponent<Player>();
+        try
+        {
+            player = GameObject.FindObjectOfType<Player>().GetComponent<Player>();
+        }
+        catch
+        {
+            Debug.Log("NO PLAYER FOUND!");
+        }
+
+        TextMeshProUGUI[] texts = this.GetComponentsInChildren<TextMeshProUGUI>();
+
+        for (int x = 0; x < texts.Length; x++)
+        {
+            if (texts[x].CompareTag("CropFertilizationText"))
+            {
+                fertilizationText = texts[x];
+            }
+            else
+            {
+                maturityText = texts[x];
+            }
+        }
+
+        //fertilizationText.text = "FERTILIZATION";
+        maturityText.text = "Score:\n0";
+        maturityText.gameObject.SetActive(false);
+        //fertilizationText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -66,7 +107,7 @@ public class CropController : MonoBehaviour
     {
         if (!isPlanted && currentSeason == Season.fall)
         {
-            Debug.Log("PLANTING from plantCrop()");
+            if (DEBUG) Debug.Log("PLANTING from plantCrop()");
             isPlanted = true;
             isAlive = true;
             int randomSprite = Random.Range(0, plantedCropSprites.Length);
@@ -81,13 +122,22 @@ public class CropController : MonoBehaviour
     public void addFertilizer()
     {
         //Debug.LogFormat("ADDING {0.0f} FERTILIZER AND REMOVING FROM PLAYER", baseFertilizerAddAmount);
+
+        if (isFamished)
+        {
+            isFamished = false;
+        }
+
         Fertilization += player.RemoveFood(baseFertilizerAddAmount);
     }
 
     public void harvestCrop()
     {
-        //Debug.Log("HARVESTING!");
-        //Debug.LogFormat("CURRENT MATURIRY: {0}", Maturity);
+        if (DEBUG)
+        {
+            Debug.Log("HARVESTING!");
+            Debug.LogFormat("CURRENT MATURIRY: {0}", Maturity);
+        }
         ScoreCounter.instance.Score += Maturity;
         resetCrop();
     }
@@ -97,6 +147,8 @@ public class CropController : MonoBehaviour
     /// </summary>
     private void resetCrop()
     {
+        if (DEBUG) Debug.Log("RESETTING CROP");
+
         isAlive = false;
         isMature = false;
         isPlanted = false;
@@ -108,22 +160,32 @@ public class CropController : MonoBehaviour
 
 
     /// <summary>
-    /// Updates maturity depending on it's fertilization and murity rates
+    /// Updates maturity depending on it's fertilization and maturity rates
     /// </summary>
     private void maturityIncrease()
     {
-        if (Fertilization > 0)
+        //if (Fertilization > 0)
+        //{
+        //    Maturity += maturityIncreaseRate + fertilizedIncreaseRate;
+        //}
+        //else
+        //{
+        //    Maturity += maturityIncreaseRate - fertilizedDecreaseRate;
+        //}
+
+        if (!isFamished)
         {
-            Maturity += maturityIncreaseRate + fertilizedIncreaseRate;
+            Maturity += maturityIncreaseRate;
         }
         else
         {
-            Maturity += maturityIncreaseRate - fertilizedDecreaseRate;
+            Maturity += (maturityIncreaseRate / 2);
         }
-        //Debug.LogFormat("Current Maturity: {0}", Maturity);
+
+        maturityText.text = "Score:\n" + Maturity.ToString();
         if (Maturity >= cropMaturityLevel && !isMature)
         {
-            //Debug.Log("MATURING!");
+            if (DEBUG) Debug.Log("MATURING!");
             changeCropToMature();
         }
     }
@@ -151,17 +213,43 @@ public class CropController : MonoBehaviour
     /// </summary>
     void OnMouseDown()
     {
-        if(!isPlanted && currentSeason == Season.fall)
+        Debug.Log("CLICKED CROP");
+
+        print("isPlanted: " + isPlanted);
+        print("currentSeason: " + currentSeason);
+        if(!isPlanted)
         {
-            //Debug.Log("PLANTING CROP");
+            if (DEBUG) Debug.Log("PLANTING CROP");
             plantCrop();
         }
-        else if(isPlanted && isAlive && (currentSeason == Season.winter || currentSeason == Season.spring))
+        else if(isPlanted && (currentSeason == Season.winter || currentSeason == Season.spring))
         {
-            //Debug.Log("ADDING FERTILIZER");
+            Debug.Log("ADDING FERTILIZER");
             addFertilizer();
         }
-        else if(isPlanted && isAlive && currentSeason == Season.summer)
+        else if(isPlanted && currentSeason == Season.summer)
+        {
+            harvestCrop();
+        }
+    }
+
+    public void interact()
+    {
+        Debug.Log("CLICKED CROP");
+
+        print("isPlanted: " + isPlanted);
+        print("currentSeason: " + currentSeason);
+        if (!isPlanted)
+        {
+            if (DEBUG) Debug.Log("PLANTING CROP");
+            plantCrop();
+        }
+        else if (isPlanted && (currentSeason == Season.winter || currentSeason == Season.spring))
+        {
+            Debug.Log("ADDING FERTILIZER");
+            addFertilizer();
+        }
+        else if (isPlanted && currentSeason == Season.summer)
         {
             harvestCrop();
         }
@@ -182,7 +270,7 @@ public class CropController : MonoBehaviour
     /// </summary>
     IEnumerator UpdateCropStatus()
     {
-        Debug.Log("STARTING UPDATE CROP STATUS");
+        if (DEBUG) Debug.Log("STARTING UPDATE CROP STATUS");
         for(; ; )
         {
             if (!isAlive)
@@ -191,11 +279,46 @@ public class CropController : MonoBehaviour
             }
             maturityIncrease();
             decreaseFertilization();
+
+            if (isPlanted && (currentSeason == Season.winter))
+            {
+                if ( famishCropChance > Random.Range(0.0f, 1.0f)) famishCrop();
+            }
+
+            if (isPlanted && (currentSeason == Season.spring))
+            {
+                if (crowAppearChance > Random.Range(0.0f, 1.0f)) crowAppear();
+            }
+
             yield return new WaitForSeconds(updateCropSeconds);
         }
     }
 
+    private void famishCrop()
+    {
 
+    }
 
+    private void crowAppear()
+    {
 
+    }
+
+    public void OnMouseEnter()
+    {
+        //fertilizationText.gameObject.SetActive(true);
+        maturityText.gameObject.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        //fertilizationText.gameObject.SetActive(false);
+        maturityText.gameObject.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision!");
+        Debug.Log(other.gameObject.tag.ToString());
+    }
 }
