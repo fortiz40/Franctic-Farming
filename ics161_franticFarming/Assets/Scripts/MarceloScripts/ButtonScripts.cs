10e3d9105bc401da70fff38fcdbf625edf1b08fe
Marcelo Pulido <mipulido@uci.edu>
2019-03-14 17:32:24 -0700

    Added a main menu and Scenes that explain the goal/mechanics

--- ics161_franticFarming/Assets/Scripts/MarceloScripts/ButtonScripts.cs
+++ ics161_franticFarming/Assets/Scripts/MarceloScripts/ButtonScripts.cs
@@ -5,29 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScripts : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     // Public functions
-    public void RestartLevel()
+    public void StartGame()
     {
-        //Time.timeScale = 1f;
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void ControlsMenu(int menuNumber)
+    {
+        switch (menuNumber)
+        {
+            case 0:
+                SceneManager.LoadScene("Controls-0");
+                break;
+            case 1:
+                SceneManager.LoadScene("Controls-1");
+                break;
+            case 2:
+                SceneManager.LoadScene("Controls-2");
+                break;
+            case 3:
+                SceneManager.LoadScene("Controls-3");
+                break;
+            case 4:
+                SceneManager.LoadScene("Controls-4");
+                break;
+            default:
+                throw new System.Exception("No condition set for int " + menuNumber);
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RestartLevel()
+    {
+        //Time.timeScale = 1f;
+        SceneManager.LoadScene("MainScene");
     }
 }
