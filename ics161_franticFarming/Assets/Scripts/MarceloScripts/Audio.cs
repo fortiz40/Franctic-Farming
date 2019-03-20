0c0574123c0bdddc25045604ed7e62c057a1297d
Marcelo Pulido <mipulido@uci.edu>
2019-03-19 17:51:46 -0700

    Added the "Audio" script, which allows for music to transcend Scenes
    - Added the "Audio" script and the "AudioManager" prefab, which allows for music to transcend Scenes.

---
+++ ics161_franticFarming/Assets/Scripts/MarceloScripts/Audio.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    // Singleton
+    public static Audio instance;
+
+    // Variables
+    [SerializeField]
+    private AudioClip music = null;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Enforce Singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Initialize variables
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Private functions
+    private void PlayMusic()
+    {
+        audioSource.clip = music;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
